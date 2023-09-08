42d0500849be8170480d95463b7d1d9fbc3c5c3a
Hamza Ghafoor <106840364+HZG4@users.noreply.github.com>
2023-09-08 19:39:51 +0500

    Update main.py

--- Day 8 Project Hangman/main.py
+++ Day 8 Project Hangman/main.py
@@ -19,27 +19,38 @@ display_word = " ".join(display_array)
 
 # Displayed UI
 clear()
-print(logo)
-print(display_word)
+print(logo + "\n")
+print(display_word + "\n")
+print("Welcome to hangman!")
+print(stages[6])
 
 continue_game = True
 lives = 6
 
+guessed_list = [""]
+
 while continue_game is True:
     guess = input("Guess a letter: ").lower()
+
     # check if guess exists and replace display
     for i in range(0, word_length):
         if guess == chosen_word[i]:
+
             display_array[i] = guess
         else:
             continue
 
+    display_word = " ".join(display_array)
     clear()
     print(logo + "\n")
-    print(display_word)
+    print(display_word + "\n")
+
     if guess not in chosen_word:
         lives = lives - 1
-        print(f"You guessed {guess}. Thats not in the word. You lose a life.")
+        print(f"You guessed {guess} wrongly. You lose a life.")
+    else:
+        print("You guessed {guess} correctly.")
+
     print(stages[lives])
 
     # check if all _ has been replaced
