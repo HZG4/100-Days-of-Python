98e0b4a189f8ade005ca2f6264358b442f2872bf
Hamza Ghafoor <106840364+HZG4@users.noreply.github.com>
2023-09-08 19:16:36 +0500

    Add files via upload

---
+++ Day 8 Project Hangman/main.py
@@ -0,0 +1,51 @@
+import random
+import os
+from hangman_words import word_list
+from hangman_ascii import stages, logo
+
+
+def clear():
+    os.system("clear")
+
+
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+# Creating list of '_'
+display_array = []
+for letter in chosen_word:
+    display_array.append("_")
+display_word = " ".join(display_array)
+
+# Displayed UI
+clear()
+print(logo)
+print(display_word)
+
+continue_game = True
+lives = 6
+
+while continue_game is True:
+    guess = input("Guess a letter: ").lower()
+    # check if guess exists and replace display
+    for i in range(0, word_length):
+        if guess == chosen_word[i]:
+            display_array[i] = guess
+        else:
+            continue
+
+    clear()
+    print(logo + "\n")
+    print(display_word)
+    if guess not in chosen_word:
+        lives = lives - 1
+        print(f"You guessed {guess}. Thats not in the word. You lose a life.")
+    print(stages[lives])
+
+    # check if all _ has been replaced
+    if "_" not in display_array:
+        print("You won the game!")
+        continue_game = False
+    if lives == 0:
+        print(f"You have lost. The word was {chosen_word}")
+        continue_game = False
