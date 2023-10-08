0e69eb491fa1961fa2245d5b524060592470005a
Hamza Ghafoor <106840364+HZG4@users.noreply.github.com>
2023-10-08 18:28:33 +0500

    updated main.py

--- Day 36 Stock Trading News Alert/main.py
+++ Day 36 Stock Trading News Alert/main.py
@@ -2,14 +2,19 @@
 import os
 import requests
 import ssl, smtplib
+from dotenv import load_dotenv
+
+load_dotenv(".env")
 
 # Constants
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
+EMAIL = os.getenv("email")
+PASSWORD = os.getenv("password")
 
 # Alpha Vantage API Constants
 AV_ENDPOINT = "https://www.alphavantage.co/query"
-AV_API_KEY = os.getenv("AV_API")
+AV_API_KEY = os.getenv("AV_API_KEY")
 AV_PARAMS = {
     "function": "TIME_SERIES_DAILY",
     "symbol": STOCK,
@@ -21,55 +26,36 @@ AV_PARAMS = {
 
 # News API Constants
 NEWS_API_ENDPOINT = "https://newsapi.org/v2/everything"
-NEWS_API_KEY = os.getenv("NEWS_API")
+NEWS_API_KEY = os.getenv("NEWS_API_KEY")
 NEWS_API_PARAMS = {
     "q": COMPANY_NAME,
     "apiKey": NEWS_API_KEY,
 }
 
-# SMTP credentials
-EMAIL = "testsubject01@gmail.com"
-PASSWORD = "xxxxxxxxxxxxxx"
-
-# Request to Alpha Vantage API and
-# retrieve closing stock price of yesterday and day before yesterday.
-av_response = requests.get(AV_ENDPOINT, params=AV_PARAMS)
-av_response.raise_for_status()
+# Get API Responses
+av_response = requests.get(url=AV_ENDPOINT, params=AV_PARAMS)
 av_data = av_response.json()
-recent_date = list(av_data["Time Series (Daily)"].keys())[0]
-prev_date = list(av_data["Time Series (Daily)"].keys())[1]
 
-recent_close = round(float(av_data["Time Series (Daily)"][recent_date]["4. close"]))
-prev_close = round(float(av_data["Time Series (Daily)"][prev_date]["4. close"]))
+lastest_stock = list(av_data["Time Series (Daily)"].keys())[0]
+previous_stock = list(av_data["Time Series (Daily)"].keys())[1]
 
+lastest_stock_price = float(av_data["Time Series (Daily)"][lastest_stock]['4. close'])
+previous_stock_price = float(av_data["Time Series (Daily)"][previous_stock]['4. close'])
 
-# Find out percentage change
-stock_price_diff = recent_close - prev_close
-percentage_change = round(stock_price_diff / prev_close * 100)
+news_response = requests.get(url=NEWS_API_ENDPOINT, params=NEWS_API_PARAMS)
+news_data = news_response.json()
+news = news_data['articles'][:3]
 
-# Check whether the stock price increased/decreased by 5%
-if percentage_change >= 5 or percentage_change <= -5:
-
-    # Request to News API to retrieve first 3 news of the company
-    news_api_response = requests.get(url=NEWS_API_ENDPOINT, params=NEWS_API_PARAMS)
-    news_api_response.raise_for_status()
-    news_api_data = news_api_response.json()
-
-    # Retrieve first 3 news of the company
-    news = [each_news for each_news in news_api_data["articles"][:3]]
-    print(news)
-    news_body = ""
-    for msg in news:
-        news_body += "".join(f"Headline: {msg['title']}\n"
-                             f"Brief: {msg['description']}\n")
-
-    server = smtplib.SMTP('smtp.gmail.com', port=587)
+five_percent = 0.05 * previous_stock_price
+if (lastest_stock_price - previous_stock_price >= five_percent) or (previous_stock_price - lastest_stock_price >= five_percent):
     context = ssl.create_default_context()
+    server = smtplib.SMTP('smtp.gmail.com', port=587)
     server.starttls(context=context)
     server.login(user=EMAIL, password=PASSWORD)
-    server.sendmail(from_addr=EMAIL,
-                    to_addrs="hamzaghafoor719@gmail.com",
-                    msg=news_body)
-
+    for each_news in news:
+        server.sendmail(from_addr=EMAIL,
+                        to_addrs="mhamzaghafoor@gmail.com",
+                        msg="Subject: Stock Fluctuation\n\n"
+                            f"Title:{each_news['title']}\n{each_news['description']}")
 else:
-    print("There was no significant change in the stock price.")
+    print("There are no stock fluctuations")
