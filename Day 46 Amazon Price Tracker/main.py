44028fa08a38cf32db9205751a3a64127c5bc3fd
Hamza Ghafoor <106840364+HZG4@users.noreply.github.com>
2023-10-18 23:09:57 +0500

    Added SMTP module

--- Day 46 Amazon Price Tracker/main.py
+++ Day 46 Amazon Price Tracker/main.py
@@ -2,6 +2,8 @@ import os
 from dotenv import load_dotenv
 from bs4 import BeautifulSoup
 import requests
+import smtplib
+import ssl
 
 load_dotenv(".env")
 EMAIL = os.getenv("EMAIL")
@@ -18,3 +20,16 @@ soup = BeautifulSoup(response.text, "lxml")
 
 product_title = str(soup.find(name="span", id="productTitle", class_="a-size-large product-title-word-break").getText()).strip().split(',')[0]
 price = float(soup.find(name= "span", class_="a-price-whole").getText())
+
+if price < 100:
+    with smtplib.SMTP("smtp.gmail.com", port=587) as server:
+        context = ssl.create_default_context()
+        server.login(user=EMAIL, password=PASSWORD)
+        server.starttls(context=context)
+        server.sendmail(from_addr=EMAIL,
+                        to_addrs="mhamzaghafoor@gmail.com",
+                        msg=f"Subject: Discount Available\n"
+                            f"Product Title: {product_title}"
+                            f"This Product is available for {price}")
+else:
+    pass
