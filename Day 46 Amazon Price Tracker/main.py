75cda046119319b2725c7cfaf0abfd36609e5958
Hamza Ghafoor <106840364+HZG4@users.noreply.github.com>
2023-10-18 23:09:17 +0500

    Added Project Directory

---
+++ Day 46 Amazon Price Tracker/main.py
@@ -0,0 +1,20 @@
+import os
+from dotenv import load_dotenv
+from bs4 import BeautifulSoup
+import requests
+
+load_dotenv(".env")
+EMAIL = os.getenv("EMAIL")
+PASSWORD = os.getenv("PASSWORD")
+
+Product_URL = "https://www.amazon.com/dp/B075CYMYK6?psc=1&ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6"
+headers = {
+    "User-Agent": "Defined",
+    "Accept-Language": "en-US,en;q=0.5"
+    }
+
+response = requests.get(url=Product_URL, headers= headers)
+soup = BeautifulSoup(response.text, "lxml")
+
+product_title = str(soup.find(name="span", id="productTitle", class_="a-size-large product-title-word-break").getText()).strip().split(',')[0]
+price = float(soup.find(name= "span", class_="a-price-whole").getText())
