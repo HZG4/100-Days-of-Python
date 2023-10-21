a5fe25ba938807d7e93c780fcbe82855a1da8482
Hamza Ghafoor <106840364+HZG4@users.noreply.github.com>
2023-10-21 22:56:42 +0500

    Apply for all jobs

--- Day 49 Automated Job Application/main.py
+++ Day 49 Automated Job Application/main.py
@@ -12,8 +12,18 @@ ACCOUNT_EMAIL = os.getenv("ACCOUNT_EMAIL")
 ACCOUNT_PASSWORD = os.getenv("ACCOUNT_PASSWORD")
 PHONE = os.getenv("PHONE")
 
+def abort_application():
+    # Click Close Button
+    close_button = driver.find_element(by=By.CLASS_NAME, value="artdeco-modal__dismiss")
+    close_button.click()
 
-# Keep the browser open if the script crashes.
+    time.sleep(2)
+    # Click Discard Button
+    discard_button = driver.find_elements(by=By.CLASS_NAME, value="artdeco-modal__confirm-dialog-btn")[1]
+    discard_button.click()
+
+
+# Optional - Keep the browser open if the script crashes.
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
@@ -41,9 +51,49 @@ password_field.send_keys(ACCOUNT_PASSWORD)
 password_field.send_keys(Keys.ENTER)
 
 # CAPTCHA - Solve Puzzle Manually
-input("Press Enter when you have solved the Captcha manually")
+input("Press Enter when you have solved the Captcha")
 
 # Get Listings
 time.sleep(5)
 all_listings = driver.find_elements(by=By.CSS_SELECTOR, value=".job-card-container--clickable")
 
+# Apply for Jobs
+for listing in all_listings:
+    print("Opening Listing")
+    listing.click()
+    time.sleep(2)
+    try:
+        # Click Apply Button
+        apply_button = driver.find_element(by=By.CSS_SELECTOR, value=".jobs-s-apply button")
+        apply_button.click()
+
+        # Insert Phone Number
+        # Find an <input> element where the id contains phoneNumber
+        time.sleep(5)
+        phone = driver.find_element(by=By.CSS_SELECTOR, value="input[id*=phoneNumber]")
+        if phone.text == "":
+            phone.send_keys(PHONE)
+
+        # Check the Submit Button
+        submit_button = driver.find_element(by=By.CSS_SELECTOR, value="footer button")
+        if submit_button.get_attribute("data-control-name") == "continue_unify":
+            abort_application()
+            print("Complex application, skipped.")
+            continue
+        else:
+            # Click Submit Button
+            print("Submitting job application")
+            submit_button.click()
+
+        time.sleep(2)
+        # Click Close Button
+        close_button = driver.find_element(by=By.CLASS_NAME, value="artdeco-modal__dismiss")
+        close_button.click()
+
+    except NoSuchElementException:
+        abort_application()
+        print("No application button, skipped.")
+        continue
+
+time.sleep(5)
+driver.quit()
