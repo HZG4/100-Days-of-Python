8e4a1254aab28ca7e063e149ba8c540ae5902af1
Hamza Ghafoor <106840364+HZG4@users.noreply.github.com>
2023-10-23 23:08:20 +0500

    Update boy,py (get_internet_speed func)

--- Day 51 Tweeting Bot/bot.py
+++ Day 51 Tweeting Bot/bot.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
 
 class InternetSpeedTwitterBot():
     def __init__(self):
@@ -7,3 +9,12 @@ class InternetSpeedTwitterBot():
         self.driver = webdriver.Chrome(options=options)
 
         self.download_speed = 0
+
+    def get_internet_speed(self):
+        self.driver.get('https://www.speedtest.net/')
+        go_btn = self.driver.find_element(By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[1]/a/span[4]')
+        go_btn.click()
+        time.sleep(30)
+        self.download_speed = float(self.driver.find_element(By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[1]/div/div[2]/span').text)
+        print(f"Internet Speed: {self.download_speed}")
+        return self.download_speed
