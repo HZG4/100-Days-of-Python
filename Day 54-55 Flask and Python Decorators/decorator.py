12d40b6ccc478584fc7478524c05aaf4d3b0a694
Hamza Ghafoor <106840364+HZG4@users.noreply.github.com>
2023-10-28 01:22:16 +0500

    Update decorator.py

--- Day 54-55 Flask and Python Decorators/decorator.py
+++ Day 54-55 Flask and Python Decorators/decorator.py
@@ -1,3 +1,28 @@
+# <-------------------------------------- Basic Decorator Analogy ---------------------------------------->
+
+import time
+def decorator_function(func):
+    def wrapper_function():
+                    # do something before running the passed_function
+        start_time = time.time() 
+                    # call the passed function, add conditions or extend its behaviour
+        func()
+                    # do something after running the passed_function
+        end_time = time.time()
+        print(f"function: {func.__name__}, Speed: {end_time-start_time}")
+                    # return wrapped function without parentheses
+    return wrapper_function
+ 
+                    # this will wrap this function in decorator_function.
+@decorator_function 
+def passed_function():
+    print("This function just prints a string")
+ 
+                    # call the decorated function
+passed_function()
+
+
+# <---------------- Using Positional Arguements and Keyword Arguements in wrapper_func() ------------------> 
 class User:
     def __init__(self, name):
         self.name = name
