984327f32fc6d110f64b6de35d6866c28956502b
Hamza Ghafoor <106840364+HZG4@users.noreply.github.com>
2024-04-29 21:48:24 +0500

    update server.py

--- Day 55 High or Low/server.py
+++ Day 55 High or Low/server.py
@@ -10,3 +10,12 @@ app = Flask(__name__)
 def home():
     return "<h1>Guess a number between 0 and 9</h1>" \
            "<img src='https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif'/>"
+
+@app.route("/<int:guess>")
+def guess_number(guess):
+    if guess > num:
+        return "<h1 style='color: purple'>Too high, try again!</h1>" \
+               "<img src='https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif'/>"
+    elif guess < num:
+        return "<h1 style='color: red'>Too low, try again!</h1>"\
+               "<img src='https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif'/>"
