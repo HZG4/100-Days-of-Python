d94d16a6034bdab89c5519014660282b86deec2d
Hamza Ghafoor <106840364+HZG4@users.noreply.github.com>
2024-04-30 22:40:16 +0500

    update app.py

--- Day 57 Blog Website/app.py
+++ Day 57 Blog Website/app.py
@@ -1,1 +1,10 @@
-from flask import Flask
+from flask import Flask, render_templates
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
