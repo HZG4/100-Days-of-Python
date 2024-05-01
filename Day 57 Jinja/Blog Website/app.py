de3c094602d8888f4857ae31fe099ad287cfacb3
Hamza Ghafoor <106840364+HZG4@users.noreply.github.com>
2024-05-01 18:16:08 +0500

    update app.py

--- Day 57 Jinja/Blog Website/app.py
+++ Day 57 Jinja/Blog Website/app.py
@@ -4,3 +4,13 @@ from post import Post
 app = Flask(__name__)
 all_posts = Post()
 
+@app.route('/')
+def home():
+    return render_template("index.html", all_posts=all_posts)
+
+@app.route('/blog_post/<int:id>')
+def blog_post(id):
+    return render_template("post.html", all_posts=all_posts, id=id)
+
+if __name__ == "__main__":
+    app.run(debug=True)
